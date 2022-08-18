252ec285d7086f6a3a3be899a297fdc93218f2cb
Jody <jodfie@gmail.com>
2022-08-18 13:07:24 -0400

    updating apt commands

--- .scripts/pm_apt_repos.sh
+++ .scripts/pm_apt_repos.sh
@@ -13,9 +13,9 @@ pm_apt_repos() {
     INSTALLED_APT_TRANSPORT_HTTPS=$(apt-cache policy apt-transport-https | grep --color=never -Po 'Installed: \K.*')
     if vergt "${MINIMUM_APT_TRANSPORT_HTTPS}" "${INSTALLED_APT_TRANSPORT_HTTPS}"; then
         info "Updating repositories (before installing apt-transport-https)."
-        eval "apt-get -y update ${REDIRECT}" || fatal "Failed to get updates from apt.\nFailing command: ${F[C]}apt-get -y update"
+        eval "apt -y update ${REDIRECT}" || fatal "Failed to get updates from apt.\nFailing command: ${F[C]}apt -y update"
         info "Installing APT transport for downloading via the HTTP Secure protocol (HTTPS)."
-        eval "apt-get -y install apt-transport-https ${REDIRECT}" || fatal "Failed to install apt-transport-https from apt.\nFailing command: ${F[C]}apt-get -y install apt-transport-https"
+        eval "apt -y install apt-transport-https ${REDIRECT}" || fatal "Failed to install apt-transport-https from apt.\nFailing command: ${F[C]}apt -y install apt-transport-https"
     fi
     local MINIMUM_LIBSECCOMP2="2.4.4"
     # Note compatibility from https://wiki.alpinelinux.org/wiki/Release_Notes_for_Alpine_3.14.0
@@ -27,10 +27,10 @@ pm_apt_repos() {
         echo "deb http://deb.debian.org/debian buster-backports main" | tee -a /etc/apt/sources.list.d/buster-backports.list || error "Failed to add buster-backports repo to sources."
     fi
     info "Updating repositories."
-    eval "apt-get -y update ${REDIRECT}" || fatal "Failed to get updates from apt.\nFailing command: ${F[C]}apt-get -y update"
+    eval "apt -y update ${REDIRECT}" || fatal "Failed to get updates from apt.\nFailing command: ${F[C]}apt -y update"
     if vergt "${MINIMUM_LIBSECCOMP2}" "${INSTALLED_LIBSECCOMP2}"; then
         info "Installing libseccomp2 from buster-backports repo."
-        eval "apt-get -y install -t buster-backports libseccomp2 ${REDIRECT}" || fatal "Failed to install libseccomp2 from buster-backports repo.\nFailing command: ${F[C]}apt-get -y install -t buster-backports libseccomp2"
+        eval "apt -y install -t buster-backports libseccomp2 ${REDIRECT}" || fatal "Failed to install libseccomp2 from buster-backports repo.\nFailing command: ${F[C]}apt -y install -t buster-backports libseccomp2"
     fi
 }
 
